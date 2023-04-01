1cf4541d6b26a4ef1ff0649814b01d1a71ec62c8
maicondguerian <maicondguerian@gmail.com>
2023-03-31 23:23:41 -0300

    fixing logo by quick-fix lol

--- frontJavaInterface/src/components/header/Header.jsx
+++ frontJavaInterface/src/components/header/Header.jsx
@@ -7,33 +7,54 @@ const StyledHeader = styled.header`
     color: ${props => props.theme.colors.white};
     display: flex;
     justify-content: space-between;
+    position: relative;
+    
+        li{
+            padding: 0 2rem;
+            display:flex;
+            align-items: center;
+        }
 
-    li{
-        display: flex;
-        align-items: center;
-        justify-content:center;
-        padding: 0 2rem;
+        ul:last-of-type{
+            li{
+                display: inline;
+            }
+        }
+    
+`
+const StyledList = styled.div`
+    font-family: 'Bebas Neue', cursive;
+    font-size: 35px;
+    display: flex;
+   
+
+    span{
+       top: 0;
+       position: absolute;
+       margin-top:16px;
+       left: 0;
+       margin-left:100px;
+
     }
 `
-const StyledList = styled.li`
-    font-family: 'Bebas Neue', cursive;
-    font-size: 25px;
-`
-
-export const LogoSvg = () => {
-    return (
-        <StyledList>
-            <CiBoxList size={35} />
-            <span>controle total</span>
-        </StyledList>)
+const SvgIcon = () =>{
+    return(
+        <>
+            <StyledList>
+                <CiBoxList size={35} />
+                <span>controle total</span>
+            </StyledList>
+        </>
+    )
 }
+
 
 export const Header = () => {
     return (
         <StyledHeader>
             <ul>
                 <li>
-                    <LogoSvg />
+                    <SvgIcon />
                 </li>
 
             </ul>
