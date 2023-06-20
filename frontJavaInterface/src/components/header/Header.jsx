376e2b6244d5c49a61234f204da5e2ef0cc230d2
jeremias rincaveski <jrincaveski@gmail.com>
2023-06-19 22:24:46 -0300

    primeiro teste

--- frontJavaInterface/src/components/header/Header.jsx
+++ frontJavaInterface/src/components/header/Header.jsx
@@ -8,10 +8,11 @@ import Typography from '@mui/material/Typography';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import { MyContext } from '../../context/MyContext';
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
-import { styled, useTheme } from 'styled-components';
+import { styled } from 'styled-components';
 import { MenuItem, Paper } from "@mui/material";
 import { DropdownModal } from "../modal/Modal";
-import { ThemeProvider, createTheme, makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import { blue } from "@mui/material/colors";
 
 export const StyledButtonLogOut = styled.button`
   background-color: transparent;
@@ -22,82 +23,86 @@ export const StyledButtonLogOut = styled.button`
 export const ButtonShowLogout = ({ Icon, size, onClick =()=> {}, color, style }) => {
   return (<StyledButtonLogOut onClick={onClick} type="button">{Icon ? <Icon size={size} color={color} style={style}/> : <></>} </StyledButtonLogOut>)
 }
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 960,
-      lg: 1280,
-      xl: 1920,
-    },
-  },
-});
 
 const styles = makeStyles((theme) => ({
     root: {
       backgroundColor: '#1c1c1c',
-      [theme.breakpoints.down('xs')]: {
-        justifyContent: 'space-between',
-        backgroundColor: 'red'
-      }
-    }
+      justifyContent: 'space-between',
+      '& > div': {
+      [theme.breakpoints.down(400)]: {
+        '&:nth-of-type(1)': {
+          display: 'none',
+        },
+      },
+     },
+    },
+  //   root: {
+  //   backgroundColor: '#1c1c1c',
+  //   '& > *': {
+  //     backgroundColor: 'red',
+  //     '&:nth-of-type(2)': {
+  //       display: 'none',
+  //     },
+  //     [theme.breakpoints.down(400)]: {
+  //       '&:nth-of-type(2)': {
+  //         display: 'block',
+  //       },
+  //     },
+  //   },
+  // },
 }))
 
 
 
 export const Header = () => {
   const { userName, setIsLogged, isLogginOut, handleLogout, getList } = React.useContext(MyContext);
-
   const classes = styles()
 
   return (
-    <ThemeProvider theme={theme}>
-      <S.Content> 
-        <AppBar position="static" >
-          <Toolbar className={classes.root}>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              sx={{ mr: 0 }}
-              onClick={getList}
-            >
-              <AssignmentIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ 
-                flexGrow: 1,
-                display: { xs: 'none', sm: 'block' },
-                textTransform: 'uppercase' }
-            }
-            >
-              controle total
+    <S.Content> 
+      <AppBar position="static" >
+        <Toolbar className={classes.root}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 0 }}
+            onClick={getList}
+          >
+            <AssignmentIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ 
+              flexGrow: 1,
+              // display: { 450 : 'none', sm: 'block' },
+              textTransform: 'uppercase' }
+          }
+          >
+            controle total
+          </Typography>
+          <Box>
+            <Typography>
+              usuario logado: {userName}
+              <ButtonShowLogout
+                Icon={ isLogginOut ? IoMdArrowDropup :  IoMdArrowDropdown }
+                size={18}
+                onClick={handleLogout}
+              />
             </Typography>
-            <Box>
-              <Typography>
-                usuario logado: {userName}
-                <ButtonShowLogout
-                  Icon={ isLogginOut ? IoMdArrowDropup :  IoMdArrowDropdown }
-                  size={18}
-                  onClick={handleLogout}
-                />
-              </Typography>
-              <DropdownModal
-                isOpen={isLogginOut}       
-              >
-                <Paper>
-                  <MenuItem sx={{fontSize: 13}} onClick={() => setIsLogged(false)}>Sair</MenuItem>
-                </Paper>
-              </DropdownModal>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </S.Content>
-    </ThemeProvider>
+            <DropdownModal
+              isOpen={isLogginOut}       
+            >
+              <Paper>
+                <MenuItem sx={{fontSize: 13}} onClick={() => setIsLogged(false)}>Sair</MenuItem>
+              </Paper>
+            </DropdownModal>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </S.Content>
   )
 }
